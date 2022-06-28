b87f31642da0ede105b351d3cc0c90881477e827
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-06-28 11:25:31 +0800

    add  multi animeted list

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,110 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<AnimatedListState> _firstListKey =
+      GlobalKey<AnimatedListState>();
+  final GlobalKey<AnimatedListState> _secondListKey =
+      GlobalKey<AnimatedListState>();
+
+  final List<String> _firstList = <String>[
+    'Caelina Cicero',
+    'Lupus Lucia',
+    'Titianus Flavianus'
+  ];
+  final List<String> _secondList = <String>[
+    'Iuvenalis Martina',
+    'Domitia Faustus',
+    'Florianus Balbus'
+  ];
+
+  String _latestRemove = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: const Center(),
+      appBar: AppBar(elevation: 0, title: Text(widget.title)),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: AnimatedList(
+              key: _firstListKey,
+              initialItemCount: _firstList.length,
+              itemBuilder: (context, index, animation) =>
+                  _buildItem(_firstList[index], animation),
+            ),
+          ),
+          Expanded(
+            child: AnimatedList(
+              key: _secondListKey,
+              initialItemCount: _secondList.length,
+              itemBuilder: (context, index, animation) =>
+                  _buildItem(_secondList[index], animation),
+            ),
+          ),
+          _buildButton(),
+        ],
+      ),
     );
+  }
+
+  Widget _buildItem(String list, Animation<double> animation) => FadeTransition(
+        opacity: animation,
+        child: Container(
+          margin: const EdgeInsets.all(4),
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: ListTile(
+            leading: const CircleAvatar(child: Icon(Icons.abc)),
+            title: Text(list),
+          ),
+        ),
+      );
+
+  Widget _buildButton() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElevatedButton(
+            child: const Text('ACTION!'),
+            onPressed: () {
+              _removeItem();
+
+              _insertItem();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _insertItem() {
+    if (_latestRemove.isNotEmpty) {
+      _secondListKey.currentState!.insertItem(
+        0,
+        duration: const Duration(milliseconds: 750),
+      );
+
+      _secondList.insert(0, _latestRemove);
+
+      _latestRemove = '';
+    }
+  }
+
+  void _removeItem() {
+    if (_firstList.isNotEmpty) {
+      final int index = _firstList.length - 1;
+
+      _latestRemove = _firstList.removeAt(index);
+
+      _firstListKey.currentState!.removeItem(
+        index,
+        (context, animation) => _buildItem(_latestRemove, animation),
+        duration: const Duration(milliseconds: 750),
+      );
+    }
   }
 }
